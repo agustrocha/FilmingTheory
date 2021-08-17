089969b68a4473954d558d716b592865802c579f
agustrocha5 <agustrocha5@gmail.com>
2021-08-17 22:56:33 +0100

    [FIX] Added video

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+require('tailwindcss/colors')
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#294156'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
