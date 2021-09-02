ccf8621faf3bd5eeff2206cbc29ed396c5b0319a
agustrocha5 <agustrocha5@gmail.com>
2021-09-02 22:43:13 +0100

    [FIX] No pagination CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 require('tailwindcss/colors')
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
